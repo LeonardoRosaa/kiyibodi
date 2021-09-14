2ffa68e3c93d5f6fc610a905f563a73fe31f993a
leonardoRosaa <leonardoorosaa16@gmail.com>
2021-09-13 22:12:48 -0300

    improved keyboard controller test

---
+++ test/keyboard_controller_test.dart
@@ -0,0 +1,31 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:kiyibodi/keyboard_controller.dart';
+import 'package:kiyibodi/kiyibodi.dart';
+
+const initialText = '12345';
+
+void main() {
+  late KeyboardController controller;
+
+  setUp(() {
+    controller = KeyboardController(value: initialText);
+  });
+
+  test('should concat text', () {
+    controller.handleActionByKeyboardInputType(KeyboardInputType.eight);
+
+    expect(controller.value.text, '${initialText}8');
+  });
+
+  test('should delete last text', () {
+    controller.handleActionByKeyboardInputType(KeyboardInputType.delete);
+
+    expect(controller.value.text, '1234');
+  });
+
+  test('should clear all text', () {
+    controller.handleActionByKeyboardInputType(KeyboardInputType.longDelete);
+
+    expect(controller.value.text.isEmpty, true);
+  });
+}
