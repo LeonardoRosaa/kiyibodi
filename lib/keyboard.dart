c41d0c1057b66d0a657aa22682841e0e1715be47
leonardoRosaa <leonardoorosaa16@gmail.com>
2021-09-15 17:14:14 -0300

    improved tests

--- lib/keyboard.dart
+++ lib/keyboard.dart
@@ -39,6 +39,8 @@ class Kiyibodi extends StatefulWidget {
 }
 
 class _KiyibodiState extends State<Kiyibodi> {
+  var canEmitOnDone = true;
+
   @override
   void initState() {
     super.initState();
@@ -50,8 +52,11 @@ class _KiyibodiState extends State<Kiyibodi> {
   }
 
   void _handleOnDone() {
-    if (isLenghtEqualsKeyboardValue && widget.onDone != null) {
+    if (isLenghtEqualsKeyboardValue && widget.onDone != null && canEmitOnDone) {
       widget.onDone!(widget.keyboardController.value.text);
+      canEmitOnDone = false;
+    } else if (!isLenghtEqualsKeyboardValue) {
+      canEmitOnDone = true;
     }
   }
 
@@ -59,131 +64,142 @@ class _KiyibodiState extends State<Kiyibodi> {
     widget.keyboardController.disabledNumbers(isLenghtEqualsKeyboardValue);
   }
 
-  bool get isLenghtEqualsKeyboardValue =>
-      widget.keyboardController.value.text.length == widget.length;
-
-  
+  bool get isLenghtEqualsKeyboardValue => widget.length == null
+      ? false
+      : widget.keyboardController.value.text.length >= widget.length!;
 
   void onTap(KeyboardInputType keyboardInputType) {
-    widget.keyboardController.handleActionByKeyboardInputType(keyboardInputType);
+    widget.keyboardController
+        .handleActionByKeyboardInputType(keyboardInputType);
   }
 
   void _onLongPress() {
-    widget.keyboardController.handleActionByKeyboardInputType(KeyboardInputType.longDelete);
+    widget.keyboardController
+        .handleActionByKeyboardInputType(KeyboardInputType.longDelete);
   }
 
   @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder<KiyibodiEditingValue>(
-      valueListenable: widget.keyboardController,
-      builder: (context, editingValue, _) {
-        return Column(
-          children: [
-            Row(
-              children: [
-                Flexible(
-                  child: KeyboardInput(
-                    child: KeyboardInputLabel('1'),
-                    onTap: editingValue.disabled ? null : onTap,
-                    value: KeyboardInputType.one,
+        valueListenable: widget.keyboardController,
+        builder: (context, editingValue, _) {
+          return Column(
+            children: [
+              Row(
+                children: [
+                  Flexible(
+                    child: KeyboardInput(
+                      child: KeyboardInputLabel('1'),
+                      onTap: editingValue.disabled ? null : onTap,
+                      value: KeyboardInputType.one,
+                      key: const Key('kiyibodiInputOne'),
+                    ),
                   ),
-                ),
-                Flexible(
-                  child: KeyboardInput(
-                    child: KeyboardInputLabel('2'),
-                    onTap: editingValue.disabled ? null : onTap,
-                    value: KeyboardInputType.two,
+                  Flexible(
+                    child: KeyboardInput(
+                      child: KeyboardInputLabel('2'),
+                      onTap: editingValue.disabled ? null : onTap,
+                      value: KeyboardInputType.two,
+                      key: const Key('kiyibodiInputTwo'),
+                    ),
                   ),
-                ),
-                Flexible(
-                  child: KeyboardInput(
-                    child: KeyboardInputLabel('3'),
-                    onTap: editingValue.disabled ? null : onTap,
-                    value: KeyboardInputType.three,
+                  Flexible(
+                    child: KeyboardInput(
+                      child: KeyboardInputLabel('3'),
+                      onTap: editingValue.disabled ? null : onTap,
+                      value: KeyboardInputType.three,
+                      key: const Key('kiyibodiInputThree'),
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Flexible(
-                  child: KeyboardInput(
-                    child: KeyboardInputLabel('4'),
-                    onTap: editingValue.disabled ? null : onTap,
-                    value: KeyboardInputType.four,
+                ],
+              ),
+              Row(
+                children: [
+                  Flexible(
+                    child: KeyboardInput(
+                      child: KeyboardInputLabel('4'),
+                      onTap: editingValue.disabled ? null : onTap,
+                      value: KeyboardInputType.four,
+                      key: const Key('kiyibodiInputFour'),
+                    ),
                   ),
-                ),
-                Flexible(
-                  child: KeyboardInput(
-                    child: KeyboardInputLabel('5'),
-                    onTap: editingValue.disabled ? null : onTap,
-                    value: KeyboardInputType.five,
+                  Flexible(
+                    child: KeyboardInput(
+                      child: KeyboardInputLabel('5'),
+                      onTap: editingValue.disabled ? null : onTap,
+                      value: KeyboardInputType.five,
+                      key: const Key('kiyibodiInputFive'),
+                    ),
                   ),
-                ),
-                Flexible(
-                  child: KeyboardInput(
-                    child: KeyboardInputLabel('6'),
-                    onTap: editingValue.disabled ? null : onTap,
-                    value: KeyboardInputType.six,
+                  Flexible(
+                    child: KeyboardInput(
+                      child: KeyboardInputLabel('6'),
+                      onTap: editingValue.disabled ? null : onTap,
+                      value: KeyboardInputType.six,
+                      key: const Key('kiyibodiInputSix'),
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Flexible(
-                  child: KeyboardInput(
-                    child: KeyboardInputLabel('7'),
-                    onTap: editingValue.disabled ? null : onTap,
-                    value: KeyboardInputType.seven,
+                ],
+              ),
+              Row(
+                children: [
+                  Flexible(
+                    child: KeyboardInput(
+                      child: KeyboardInputLabel('7'),
+                      onTap: editingValue.disabled ? null : onTap,
+                      value: KeyboardInputType.seven,
+                      key: const Key('kiyibodiInputSeven'),
+                    ),
                   ),
-                ),
-                Flexible(
-                  child: KeyboardInput(
-                    child: KeyboardInputLabel('8'),
-                    onTap: editingValue.disabled ? null : onTap,
-                    value: KeyboardInputType.eight,
+                  Flexible(
+                    child: KeyboardInput(
+                      child: KeyboardInputLabel('8'),
+                      onTap: editingValue.disabled ? null : onTap,
+                      value: KeyboardInputType.eight,
+                      key: const Key('kiyibodiInputEight'),
+                    ),
                   ),
-                ),
-                Flexible(
-                  child: KeyboardInput(
-                    child: KeyboardInputLabel('9'),
-                    onTap: editingValue.disabled ? null : onTap,
-                    value: KeyboardInputType.nine,
+                  Flexible(
+                    child: KeyboardInput(
+                      child: KeyboardInputLabel('9'),
+                      onTap: editingValue.disabled ? null : onTap,
+                      value: KeyboardInputType.nine,
+                      key: const Key('kiyibodiInputNine'),
+                    ),
                   ),
-                ),
-              ],
-            ),
-            Row(
-              children: [
-                Flexible(
-                  child: widget.leftChild ?? Container(),
-                ),
-                Flexible(
-                  child: KeyboardInput(
-                    child: KeyboardInputLabel('0'),
-                    onTap: editingValue.disabled ? null : onTap,
-                    value: KeyboardInputType.zero,
+                ],
+              ),
+              Row(
+                children: [
+                  Flexible(
+                    child: widget.leftChild ?? Container(),
                   ),
-                ),
-                Flexible(
-                  child: KeyboardInput(
-                    child: widget.rightChild ??
-                        Icon(
-                          Icons.arrow_back_ios,
-                        ),
-                    onTap: onTap,
-                    value: KeyboardInputType.delete,
-                    onLongPress: (_) {
-                      _onLongPress();
-                    },
+                  Flexible(
+                    child: KeyboardInput(
+                      child: KeyboardInputLabel('0'),
+                      onTap: editingValue.disabled ? null : onTap,
+                      value: KeyboardInputType.zero,
+                      key: const Key('kiyibodiInputZero'),
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ],
-        );
-      }
-    );
+                  Flexible(
+                    child: KeyboardInput(
+                      child: widget.rightChild ??
+                          Icon(
+                            Icons.arrow_back_ios,
+                          ),
+                      onTap: onTap,
+                      value: KeyboardInputType.delete,
+                      onLongPress: (_) {
+                        _onLongPress();
+                      },
+                      key: const Key('kiyibodiInputRightChild'),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          );
+        });
   }
 }
